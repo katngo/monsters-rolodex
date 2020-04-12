69a136e834044de2248b3d4f8419324fd36e7772
katngo <katn.c9@gmail.com>
2020-04-11 18:54:52 -0700

    Updates

---
+++ precache-manifest.ba32010f341a7b2d9d9717ce287d359a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce2ad0f34259c9837c70146ce60f0be",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3db83453cd216b96a4d9",
+    "url": "/monsters-rolodex/static/css/main.b85e5496.chunk.css"
+  },
+  {
+    "revision": "f421a9324b07a9c7989c",
+    "url": "/monsters-rolodex/static/js/2.6351a390.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.6351a390.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3db83453cd216b96a4d9",
+    "url": "/monsters-rolodex/static/js/main.f7ff1988.chunk.js"
+  },
+  {
+    "revision": "d90ac9b9402da2dda087",
+    "url": "/monsters-rolodex/static/js/runtime-main.f8e4036a.js"
+  }
+]);
